e99e8efe02e09c3bdc9a02d32d9f87a0b41f984e
Wang Fengning <17749734946@163.com>
2016-12-23 09:37:56 +0800

    002

--- runoobpy002.py
+++ runoobpy002.py
@@ -1,3 +1,15 @@
 # coding:utf-8
 __author__ = '王丰宁'
 
+i= int(raw_input('净利润：'))
+arr = [10,6,4,2,1,0]
+arr = [x*100000 for x in arr]
+rat = [1, 1.5, 3, 5, 7.5, 10]
+rat = [x*0.01 for x in rat]
+r = 0
+for idx in range(0,6):
+    if i>arr[idx]:
+        j =(i - arr[idx])*rat[idx]
+        r += j
+        i = arr[idx]
+print r
